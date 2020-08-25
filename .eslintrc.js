aded9f8f290ad40d49cf9823849066d7d4fbbdfd
nishida <k.nishida@vistage-llc.com>
2020-08-25 12:12:58 +0900

    Vue Apollo Queries

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: [2, "always"],
+    quotes: ["warn", "double"],
+    "space-before-blocks": ["warn", { functions: "always" }],
+    "no-extra-semi": "warn",
+    "space-before-function-paren": ["error", "never"]
   }
-}
+};
